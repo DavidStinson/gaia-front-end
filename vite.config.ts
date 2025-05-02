b58d8d24688cc3c01dde7b49c07c51377b41da26
laurenperez <laurenashleymarks@gmail.com>
2025-05-02 13:03:33 -0700

    testing homepage

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/gaia-front-end/",
+  base: "/GA-AI-Systems/gaia-front-end/",
   plugins: [react(), tailwindcss()],
 })
